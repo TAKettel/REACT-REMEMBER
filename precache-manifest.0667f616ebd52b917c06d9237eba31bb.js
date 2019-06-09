aaf80676b4b0b60f4b0d55284ba2815d086fb1be
Trevor <takettel@hotmail.com>
2019-06-09 17:21:25 -0500

    Updates

---
+++ precache-manifest.0667f616ebd52b917c06d9237eba31bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41b38d5b6d5bddf04d9a4b1c8f05d95",
+    "url": "/react-remember/index.html"
+  },
+  {
+    "revision": "2af061cd07aefad427ca",
+    "url": "/react-remember/static/css/main.a07415ba.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-remember/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "2af061cd07aefad427ca",
+    "url": "/react-remember/static/js/main.e277ad10.chunk.js"
+  },
+  {
+    "revision": "a497a4624fd99936035f",
+    "url": "/react-remember/static/js/runtime~main.9dc561ba.js"
+  }
+]);
