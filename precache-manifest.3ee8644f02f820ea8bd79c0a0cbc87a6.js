f164d1211d7ff0ddc0abeb87c8b83c81bcce74ed
Trevor <takettel@hotmail.com>
2019-07-24 21:35:22 -0500

    Updates

--- precache-manifest.3ee8644f02f820ea8bd79c0a0cbc87a6.js
+++ precache-manifest.3ee8644f02f820ea8bd79c0a0cbc87a6.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e0f81a78f4a16aefb0008a90b5ae7064",
+    "revision": "90764eb199a96c85558b03f05d1883bd",
     "url": "/react-remember/index.html"
   },
   {
-    "revision": "b867c7f80ed22123c6d3",
-    "url": "/react-remember/static/css/main.a2ef995a.chunk.css"
+    "revision": "f1ed35523151e99abf6a",
+    "url": "/react-remember/static/css/main.6f1081d8.chunk.css"
   },
   {
     "revision": "dd0ed99a380f8d225736",
     "url": "/react-remember/static/js/2.884c28c3.chunk.js"
   },
   {
-    "revision": "b867c7f80ed22123c6d3",
-    "url": "/react-remember/static/js/main.4db0bee9.chunk.js"
+    "revision": "f1ed35523151e99abf6a",
+    "url": "/react-remember/static/js/main.36f39103.chunk.js"
   },
   {
     "revision": "a497a4624fd99936035f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-remember/precache-manifest.3a4e27c6eedef6beb71121d6a644cc5e.js"
+  "/react-remember/precache-manifest.3ee8644f02f820ea8bd79c0a0cbc87a6.js"
 );
 
 self.addEventListener('message', (event) => {
