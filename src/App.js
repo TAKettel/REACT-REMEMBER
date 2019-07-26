a2b333c8a05b9fb7658dd8658b8eb44ebdd00773
Trevor <takettel@hotmail.com>
2019-07-25 19:38:05 -0500

    updates to background sourcing

--- src/App.js
+++ src/App.js
@@ -1,103 +1,100 @@
-import React, { Component} from 'react';
-import Wrapper from './components/Wrapper';
-import StickyHeader from './components/StickyHeader';
-import CharacterBlock from './components/CharacterBlock';
-import CharacterCard from './components/CharacterCard';
+import React, { Component } from "react";
+import Wrapper from "./components/Wrapper";
+import StickyHeader from "./components/StickyHeader";
+import CharacterBlock from "./components/CharacterBlock";
+import CharacterCard from "./components/CharacterCard";
 import characters from "./characters.json";
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   state = {
     characters,
-    score: 0,
+    score: 0
   };
 
   characterArray = [];
   indexesArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-
   // score = 0;
 
   checkForDupe = id => {
-    //  i'm calling id and then calling it again inside the forEach, I need to ask if this works, or why it dosen't.    
-      //  if the new id is already in the array... game over.
-      if(this.characterArray.includes(id)) {
-        console.log("game over");
-        setTimeout(this.gameover(), 5000);
-      } else {
+    //  i'm calling id and then calling it again inside the forEach, I need to ask if this works, or why it dosen't.
+    //  if the new id is already in the array... game over.
+    if (this.characterArray.includes(id)) {
+      console.log("game over");
+      setTimeout(this.gameover(), 5000);
+    } else {
       //  ... else add to the array to be checked next time.
-        this.characterArray.push(id);
-        this.setState(prevState => {
-          return {score: prevState.score + 1}
-        })
-        console.log(this.characterArray);
-        this.render();
-      }
-  }
+      this.characterArray.push(id);
+      this.setState(prevState => {
+        return { score: prevState.score + 1 };
+      });
+      console.log(this.characterArray);
+      this.render();
+    }
+  };
 
   gameover() {
     this.characterArray = [];
-    this.setState({score: 0});
+    this.setState({ score: 0 });
     this.render();
   }
 
   shuffle(indexesArray) {
-    var i,
-        j,
-        temp;
+    var i, j, temp;
     for (i = this.state.characters.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        temp = this.indexesArray[i];
-        this.indexesArray[i] = this.indexesArray[j];
-        this.indexesArray[j] = temp;
+      j = Math.floor(Math.random() * (i + 1));
+      temp = this.indexesArray[i];
+      this.indexesArray[i] = this.indexesArray[j];
+      this.indexesArray[j] = temp;
     }
     return indexesArray;
-  };
+  }
 
   // https://gist.github.com/ecarter/1423674
-  mapOrder(array, order, key){
-    array.sort( function (a, b) {
-      var A = a[key], B = b[key];
-      
+  mapOrder(array, order, key) {
+    array.sort(function(a, b) {
+      var A = a[key],
+        B = b[key];
+
       if (order.indexOf(A) > order.indexOf(B)) {
         return 1;
       } else {
         return -1;
       }
-      
     });
-    
+
     return array;
   }
 
   render() {
     // this.shuffle(this.characters);
     this.shuffle(this.indexesArray);
-    this.mapOrder(characters, this.indexesArray, 'id')
+    this.mapOrder(characters, this.indexesArray, "id");
     return (
       <Wrapper>
-        <StickyHeader score={this.state.score}/>
+        <StickyHeader score={this.state.score} />
         {console.log(this.state.score)}
         <div className="row">
-        {/* Why aren't the Bootstrap grid elements working?  Is there something else I need to do to get a max 4-col character set? */}
-        <CharacterBlock>
-        {/* Randomizing the INDEX of the array elements. Generate an array of indexes, then switch and swap. 
+          {/* Why aren't the Bootstrap grid elements working?  Is there something else I need to do to get a max 4-col character set? */}
+          <CharacterBlock>
+            {/* Randomizing the INDEX of the array elements. Generate an array of indexes, then switch and swap. 
         Not sure that I'm doing this right just yet, or how to get the shuffled index to talk to the mapping. */}
-        {this.state.characters.map((friend, q) => (
-          <CharacterCard
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            checkForDupe={this.checkForDupe}
-          />
-        ))}
-        </CharacterBlock>
+            {this.state.characters.map((friend, q) => (
+              <CharacterCard
+                id={friend.id}
+                key={friend.id}
+                name={friend.name}
+                image={friend.image}
+                checkForDupe={this.checkForDupe}
+              />
+            ))}
+          </CharacterBlock>
         </div>
       </Wrapper>
     );
   }
-};
+}
 
 // Things that still need to happen:
 //    Get the navbar to not margin-move with the cards, and have the navbar show on top. Z-index helps with the second part, at least. DONE.
